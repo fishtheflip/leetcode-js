d769bb2255819c15498705a9cdb52da47236da54
fishtheflip <ataflava@gmail.com>
2022-11-05 23:17:00 +0600

    add description to find number

--- find-number.js
+++ find-number.js
@@ -1,13 +1,47 @@
-const plusOne = (digits)=> {
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+
+// Example 1:
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+// Example 2:
+
+// Input: digits = [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321.
+// Incrementing by one gives 4321 + 1 = 4322.
+// Thus, the result should be [4,3,2,2].
+
+// Example 3:
+
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
+
+
+
+
+// Without type convert, but faster and accentable in leetcode
+const plusOne = digits => ('' + (BigInt((digits.map((i)=> '' + i)).join('')) + BigInt(1))).split('')
+
+// With type convert
+const plusOneVersionTwo = (digits)=> {
     const digitstStrings = digits.map((i)=> '' + i)
     const digitsStringNumber = digitstStrings.join('')
     const digitsSumNumber = BigInt(digitsStringNumber) + BigInt(1)
-    const digitsSumString = '' + digitsSumNumber
-    const digitsArrSumString = digitsSumString.split('')
-    const digitstArrSumNumber = digitsArrSumString.map(i=> i)
+    const digitsArrSumString = ('' + digitsSumNumber).split('')
+    const digitstArrSumNumber = digitsArrSumString.map(i=> +i)
     return digitstArrSumNumber
 };
 
-plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3])
-plusOne([1,2,3])
-plusOne([9])
+console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
+console.log(plusOne([1,2,3]))
+console.log(plusOne([9]))
